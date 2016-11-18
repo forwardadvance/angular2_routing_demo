a1c28ca0103965c014f4f77ffeb40565c17cc866
Nicholas Johnson <nicholas@forwardadvance.com>
2016-11-18 13:57:16 +0000

    exercise

---
+++ exercise/app/home.page.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+
+@Component({
+    selector: 'catPage',
+    template: `
+      <h1>Welcome to the app</h1>
+      <p>Here is the home page</p>
+    `
+})
+export class HomePageComponent {
+}
